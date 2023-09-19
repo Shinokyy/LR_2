a2b206f2dd9b76c1df4cb3bb16ffbc03542cf921
Shinokyy <gitshinoky@mail.ru>
2023-09-19 19:35:07 +0700

    task2
    task 2/LR-2

---
+++ task2/task2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+    double a, b, c;
+    std::cin >> a;
+    b = a % 10;
+    c = a / 10 % 10;
+    std::cout << b<<c;
+    return 0;
+
+    
+    
+}
+
